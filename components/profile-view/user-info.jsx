58d9a3505b21a3c3ce71f3418d661efe617ce50a
Jorge Cortés <jorge.cortes@gmail.com>
2023-09-02 17:29:39 -0700

    fixed user profile

--- components/profile-view/user-info.jsx
+++ components/profile-view/user-info.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-function UserInfo({email, name}) {
-    return (
-        <>
-            <h4>Your Info</h4>
-            <p>User: {user.Username}</p>
-            <p>Email: {user.Email}</p>
-        </>
-    )
+function UserInfo({ email, name }) {
+  return (
+    <>
+      <h4>Your Info</h4>
+      <p>User: {name}</p>
+      <p>Email: {email}</p>
+    </>
+  );
 }
 
-export default UserInfo
+export default UserInfo;
